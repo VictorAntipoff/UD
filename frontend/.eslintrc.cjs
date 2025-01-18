a8aa2bf2ae23166a51930d1f3c091b7673f80ab9
VictorAntipoff <75905706+VictorAntipoff@users.noreply.github.com>
2025-01-18 11:46:40 +0300

    chore: setup staging environment configuration

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,35 +1,18 @@
-const js = require('@eslint/js');
-const tsParser = require('@typescript-eslint/parser');
-const tsPlugin = require('@typescript-eslint/eslint-plugin');
-
 module.exports = {
-  files: ['**/*.{js,jsx,ts,tsx}'],
-  ignores: ['dist/**', 'node_modules/**'],
-  languageOptions: {
-    parser: tsParser,
-    parserOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-      ecmaFeatures: {
-        jsx: true
-      }
-    }
-  },
-  plugins: {
-    '@typescript-eslint': tsPlugin,
-    'react-refresh': require('eslint-plugin-react-refresh')
-  },
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
   rules: {
-    ...js.configs.recommended.rules,
-    ...tsPlugin.configs.recommended.rules,
     'react-refresh/only-export-components': [
       'warn',
-      { allowConstantExport: true }
-    ]
+      { allowConstantExport: true },
+    ],
   },
-  settings: {
-    react: {
-      version: 'detect'
-    }
-  }
-}; 
+} 
